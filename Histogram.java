59fa709ae7e4d984261967adcd8701d276a02c96
Brian Schott <brian@bronxsoftware.org>
2017-12-13 06:50:52 -0500

    Create Histogram.java

---
+++ Histogram.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Histogram
+{
+	//add an int[] array of length 10 that counts the number of times each numeral appears in an inputted array
+  //for instance, array[0] should be the number of times a 0 appears in the counted array, array[1] should be
+  // the number of times a 1 appears in the counted array, and so on.
+
+  //add an int[] array property representing the array to be counted
+  
+  //add a constructor
+  
+  //add a set method that assigns the parameter to the array to be counted
+  
+  //add a generateHistogram method that counts the number of times each numeral appears in the toBeCounted array
+  //this will use a for loop
+  //there are several ways to do this :) 
+  
+  
+	//toString method simply returns the array of length 10 discussed at the beginning of this file
+}
