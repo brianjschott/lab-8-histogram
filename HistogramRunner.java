7d1a2b647647902ecb8bb69ab2cccf703bd4d545
Brian Schott <brian@bronxsoftware.org>
2017-12-13 06:41:47 -0500

    HistogramRunner.java

---
+++ HistogramRunner.java
@@ -0,0 +1,13 @@
+public class HistogramRunner
+{
+	public static void main(String args[])
+	{
+  //create an array of integers
+  
+  //create a histogram object with the array as a parameter
+  
+  //Print the histogram object (this will call the .toString() method
+  
+  
+	}
+}
